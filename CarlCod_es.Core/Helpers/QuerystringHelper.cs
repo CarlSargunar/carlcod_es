8661944b92528d1be2f76f08e4ee5bae8144c972
Carl Sargunar <carl@sargunar.com>
2022-11-12 20:40:35 +0000

    wip

---
+++ CarlCod_es.Core/Helpers/QuerystringHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarlCod_es.Core.Helpers
+{
+    public static class QueryStringHelper
+    {
+        public static int GetIntFromQueryString(HttpRequestBase request, string key, int fallbackValue = 0)
+        {
+            var stringValue = request.QueryString[key];
+            if (stringValue != null && !string.IsNullOrWhiteSpace(stringValue) && int.TryParse(stringValue, out var numericValue))
+            {
+                return numericValue;
+            }
+            return fallbackValue;
+        }
+    }
+}
